f8ffcb581dfba4e06d1ec104ce71e5a71b4840c9
leskerito <franck.kindia@outlook.fr>
2025-05-26 17:44:23 +0200

    Site is functional on mobile and web, needs to touch up UX/UI on mobile

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//Window buttons definition
 var closeButtons = document.getElementsByClassName("close");
-var minimizeButtons = document.getElementsByClassName("Minimize");
-var maximizeButtons = document.getElementsByClassName("Maximize");
-var titles = document.getElementsByClassName("title-bar-text");
-var folderButtons = document.getElementsByClassName("folder-button");
+var minimizeButtons = document.getElementsByClassName("minimize");
+var maximizeButtons = document.getElementsByClassName("maximize");
 
-console.log(closeButtons);
+// Desktop icons and folder items definition
+var buttons = Array.from(document.getElementsByClassName("button"));
+var folderItems = Array.from(document.querySelectorAll(".folder-item"));
+
+//Verify if the browser is mobile or desktop
 function isMobile() {
   return (
     /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) ||
@@ -12,27 +15,59 @@ function isMobile() {
   );
 }
 
+//Creates the window buttons functionality and removes the minimize and maximize buttons on mobile
 function createWindowButtons() {
   for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener("click", function () {
-      const allFolders = document.querySelectorAll(".folder");
-      allFolders.forEach((f) => f.classList.remove("show"));
-    });
-    titles[i].innerHTML = titles[i].parentElement.parentElement.id.replace(
-      "Folder",
-      ""
-    );
-
     if (isMobile()) {
       minimizeButtons[i].style.display = "none";
       maximizeButtons[i].style.display = "none";
     }
+
+    closeButtons[i].addEventListener("click", function () {
+      const allFolders = document.querySelectorAll(".folder");
+      allFolders.forEach((f) => f.classList.remove("show"));
+    });
   }
 }
+
+//If the browser is mobile, will allow us to click once to open a link or folder
+//If the browser is desktop, will allow us to click once to select and double click to open a link or folder
+if (isMobile()) {
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      if (button.classList.contains("desktopIcon")) {
+        openFolder(button.id);
+      } else {
+        window.open(button.id, "_blank").focus();
+      }
+    });
+  });
+} else {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      button.focus();
+      button.classList.add("active");
+      buttons.forEach((b) => {
+        if (b !== button) {
+          b.classList.remove("active");
+        }
+      });
+    });
+
+    button.addEventListener("dblclick", (event) => {
+      if (button.classList.contains("desktopIcon")) {
+        openFolder(button.id);
+      } else {
+        window.open(button.id, "_blank").focus();
+      }
+    });
+  });
+}
+
+// Call the function to create window buttons
 createWindowButtons();
 
-var folderBars = document.getElementsByClassName("folder-bar");
-
+// Function to open a folder by its ID
 function openFolder(folder) {
   const allFolders = document.querySelectorAll(".folder");
   allFolders.forEach((f) => {
