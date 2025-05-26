0e95e1cb0465805621c0d82fa16b5961d79971d9
leskerito <franck.kindia@outlook.fr>
2025-05-26 09:01:13 +0200

    added dbl click, icons and link

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 var closeButtons = document.getElementsByClassName("close");
+
+function isMobile() {
+  return (
+    /iPhone|iPad|iPod|Android/i.test(navigator.userAgent) ||
+    (window.innerWidth <= 800 && window.innerHeight <= 600)
+  );
+}
+
 for (var i = 0; i < closeButtons.length; i++) {
   closeButtons[i].addEventListener("click", function () {
     const allFolders = document.querySelectorAll(".folder");
@@ -6,10 +14,17 @@ for (var i = 0; i < closeButtons.length; i++) {
   });
 }
 
+console.log("Is mobile: " + isMobile());
+
+var folderBars = document.getElementsByClassName("folder-bar");
+for (var i = 0; i < folderBars.length; i++) {
+  console.log(folderBars[i].parentElement.id);
+}
+
 function openFolder(folder) {
   const allFolders = document.querySelectorAll(".folder");
   allFolders.forEach((f) => {
-    console.log(f);
+    f.classList.remove("show");
   });
 
   const opened = document.getElementById(folder + "Folder");
